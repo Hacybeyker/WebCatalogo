dbf258c1f4539572afa4998f8a995054ba7ecd5b
Carlos Alfredo Osorio Perez <Hacybeyker@hotmail.com>
2016-03-21 22:18:39 -0500

    Proyecto con arquitectura N-Capas

---
+++ Modelo/c3_dominio/entidad/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.c3_dominio.entidad
+{
+    public class Usuario
+    {
+        public int codigoUsuario { get; set; }
+        public string nombreUsuario { get; set; }
+        public string cuentaUsuario { get; set; }
+        public string claveUsuario { get; set; }
+        public Usuario()
+        {
+            this.codigoUsuario = 0;
+        }
+    }
+}
---
+++ Modelo/c4_persistencia/sqlserver/ConexionSqlServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Modelo.c5_transversal.excepcion;
+
+namespace Modelo.c4_persistencia.sqlserver
+{
+    public class ConexionSqlServer : GestorODBC
+    {
+        public override void abrirConexion()
+        {
+            try
+            {
+                string url = "data source=Hacybeyker;initial catalog=paginaweb3;persist security info=True;user id=sa;password=123;MultipleActiveResultSets=True;";
+                conexion = new SqlConnection(url);
+                conexion.Open();
+            }
+            catch (Exception)
+            {
+                throw ExcepcionSQL.crearErrorAbrirConexion(); ;
+            }
+        }
+    }
+}
